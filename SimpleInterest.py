efc2d7fbe71849825cac6a58e9ceac71e606bd36
prajjwalRathore-39 <prajjwalrathore39@gmail.com>
2024-10-18 12:26:34 +0530

    Simple Interest

---
+++ SimpleInterest.py
@@ -0,0 +1,7 @@
+principal = int(input("Enter the value for principal."))
+rate = int(input("Enter the rate value"))
+time = int(input("Enter the time in years"))
+
+interest = (principal*rate*time)/100
+
+print("The Interest to be paid is : ", interest)
